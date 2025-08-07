c7020e69bba6f618d9f27ad1defa02afc9764550
Lucas Pirez <lucaspirez42@gmail.com>
2025-08-07 12:58:52 -0300

    add Controller AuthController

---
+++ Backend/API/Controllers/AuthController.cs
@@ -0,0 +1,59 @@
+﻿using Application.Dtos.Auth;
+using Application.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("auth")]
+    public class AuthController : ControllerBase
+    {
+        private readonly AuthService _authService;
+
+        public AuthController(AuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] AuthRequestDto request)
+        {
+            var response = await _authService.LoginAsync(request);
+
+            return Ok(response);
+        }
+
+        [HttpPatch("request-reset-password")]
+        public async Task<IActionResult> RequestResetPassword(
+            [FromBody] RequestResetPasswordDto dto
+        )
+        {
+            var admin = await _authService.GetAdminByEmailAsync(dto.Email);
+
+            _ = _authService
+                .SendTokenResetPassword(email: dto.Email, baseLink: dto.Link)
+                .ContinueWith(
+                    task =>
+                    {
+                        if (task.Exception != null)
+                        {
+                            Console.WriteLine(
+                                task.Exception.ToString() + "Error al enviar notificación"
+                            );
+                        }
+                    },
+                    TaskContinuationOptions.OnlyOnFaulted
+                );
+
+            return Accepted();
+        }
+
+        [HttpPatch("change-password")]
+        public async Task<IActionResult> ChangePassword([FromBody] ChangePasswordDto dto)
+        {
+            await _authService.ChangePassword(dto.ResetPasswordToken, dto.Password);
+
+            return Ok();
+        }
+    }
+}
